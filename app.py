bb89b7857e919c825fc377808df3af84f3947831
Albert Le <albert.l1399@gmail.com>
2023-10-14 13:59:13 -0700

    import openai & SQLAlchemy

--- app.py
+++ app.py
@@ -2,14 +2,18 @@
 
 from flask import Flask,request,render_template
 from json import loads,dumps,load
+import openai
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route('/')
-def hello():
-    if 'use_template' in request.args:
-        return render_template('hello.html')
-    else:
-        return 'Hello World!'
+# @app.route('/')
+# def hello():
+#     if 'use_template' in request.args:
+#         return render_template('hello.html')
+#     else:
+#         return 'Hello World!fdsaf'
 
+@app.route('/', methods=['GET', 'POST'])
+def openai():
+    return render_template('openai.html')
