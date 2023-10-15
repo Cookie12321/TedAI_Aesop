c64deaf21452eda49d36d42646686d94128bdf12
Albert Le <albert.l1399@gmail.com>
2023-10-15 02:23:05 -0700

    add scores table

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask,request,render_template
-from helper_functions import initialize_prompt_db, insert_into_prompt_db, view_prompts_db, generate_story, psychoanalyze_prompt_responses
+from helper_functions import initialize_db, insert_to_prompts_table, view_prompts_table, view_scores_table, generate_story, psychoanalyze_prompt_responses
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -9,7 +9,7 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    initialize_prompt_db()
+    initialize_db()
     return render_template('home.html')
 
 
@@ -27,10 +27,10 @@ def missing_parts():
         p3 = request.form.get("prompt_3_answer")
         p4 = request.form.get("prompt_4_answer")
         p5 = request.form.get("prompt_5_answer")
-        insert_into_prompt_db("missing_parts", p1 + p2 + p3 + p4 + p5)
+        insert_to_prompts_table("missing_parts", p1 + p2 + p3 + p4 + p5)
         story = generate_story(q1 + p1 + q2 + p2 + q3 + p3 + q4 + p4 + q5 + p5)
         scores = psychoanalyze_prompt_responses(p1 + p2 + p3 + p4 +p5)
-        return f"{story}<br><br>Prompt answer analysis: {scores}<br><br>See database: http://localhost:8080/prompts_db"
+        return f"{story}<br><br>Prompt answer analysis: {scores}<br><br>See prompts database: http://localhost:8080/get_prompt_results<br><br>See scores database:http://localhost:8080/get_scores"
 
     return render_template('missing_parts.html')
 
@@ -49,14 +49,19 @@ def broken_bridge():
         p3 = request.form.get("prompt_3_answer")
         p4 = request.form.get("prompt_4_answer")
         p5 = request.form.get("prompt_5_answer")
-        insert_into_prompt_db("broken_bridge", p1 + p2 + p3 + p4 + p5)
+        insert_to_prompts_table("broken_bridge", p1 + p2 + p3 + p4 + p5)
         story = generate_story(q1 + p1 + q2 + p2 + q3 + p3 + q4 + p4 + q5 + p5)
         scores = psychoanalyze_prompt_responses(p1 + p2 + p3 + p4 + p5)
-        return f"{story}<br><br>Prompt answer analysis: {scores}<br><br>See database: http://localhost:8080/prompts_db"
+        return f"{story}<br><br>Prompt answer analysis: {scores}<br><br>See prompts database: http://localhost:8080/get_prompt_results<br><br>See scores database:http://localhost:8080/get_scores"
 
     return render_template('broken_bridge.html')
 
 
-@app.route('/prompts_db', methods=['GET'])
-def prompts_db():
-    return view_prompts_db()
+@app.route('/get_prompt_results', methods=['GET'])
+def get_prompt_results_from_db():
+    return view_prompts_table()
+
+
+@app.route('/get_scores', methods=['GET'])
+def get_scores_from_db():
+    return view_scores_table()
--- helper_functions.py
+++ helper_functions.py
@@ -6,26 +6,75 @@ import json
 API_KEY = ""
 
 
-def initialize_prompt_db():
+def initialize_db():
     with sqlite3.connect('mydb.sqlite') as conn:
         cur = conn.cursor()
         cur.execute("drop table if exists prompt_results")
         cur.execute('create table prompt_results (id integer primary key, category text, prompt_answers text)')
+        cur.execute("drop table if exists scorsg_scores")
+        cur.execute('create table scorsg_scores (com tinyint, aff tinyint, eir tinyint, eim tinyint, sc tinyint, agg tinyint, se tinyint, ics tinyint, insert_ts timestamp default current_timestamp)')
         conn.commit()
 
 
-def insert_into_prompt_db(category, prompt_answers):
+def insert_to_prompts_table(category, prompt_answers):
     with sqlite3.connect('mydb.sqlite') as conn:
         cur = conn.cursor()
         cur.execute(f"insert into prompt_results (category, prompt_answers) values ('{category}', '{prompt_answers}')")
         conn.commit()
 
+    
+def insert_to_scores_table(scoring):
+        json_scores = json.loads(scoring)
+        scores = {}
+        scores["com"] = json_scores["COM"]["score"]
+        scores["aff"] = json_scores["AFF"]["score"]
+        scores["eir"] = json_scores["EIR"]["score"]
+        scores["eim"] = json_scores["EIM"]["score"]
+        scores["sc"] = json_scores["SC"]["score"]
+        scores["agg"] = json_scores["AGG"]["score"]
+        scores["se"] = json_scores["SE"]["score"]
+        scores["ics"] = json_scores["ICS"]["score"]
 
-def view_prompts_db():
+        with sqlite3.connect('mydb.sqlite') as conn:
+            cur = conn.cursor()
+            cur.execute(f"insert into scorsg_scores (com, aff, eir, eim, sc, agg, se, ics) values ({scores['com']},{scores['aff']},{scores['eir']},{scores['eim']},{scores['sc']},{scores['agg']},{scores['se']},{scores['ics']})")
+            conn.commit()
+
+
+def view_prompts_table():
     with sqlite3.connect('mydb.sqlite') as conn:
         cur = conn.cursor()
         res = cur.execute('select * from prompt_results')
         return res.fetchall()
+    
+
+def view_scores_table(limit=20):
+    final_json = {}
+    final_json["COM"] = []
+    final_json["AFF"] = []
+    final_json["EIR"] = []
+    final_json["EIM"] = []
+    final_json["SC"] = []
+    final_json["AGG"] = []
+    final_json["SE"] = []
+    final_json["ICS"] = []
+    
+    with sqlite3.connect('mydb.sqlite') as conn:
+        cur = conn.cursor()
+        res = cur.execute(f'select com, aff, eir, eim, sc, agg, se, ics from scorsg_scores order by insert_ts asc limit {limit}')
+        results = res.fetchall()
+        
+        for row in results:
+            final_json["COM"].append(row[0])
+            final_json["AFF"].append(row[1])
+            final_json["EIR"].append(row[2])
+            final_json["EIM"].append(row[3])
+            final_json["SC"].append(row[4])
+            final_json["AGG"].append(row[5])
+            final_json["SE"].append(row[6])
+            final_json["ICS"].append(row[7])
+
+    return final_json
         
 
 def generate_story(combined_prompts_and_answers):
@@ -67,4 +116,5 @@ def psychoanalyze_prompt_responses(combined_prompt_answers):
         ]
     )
     scoring = chat_completion['choices'][0]['message']['content']
+    insert_to_scores_table(scoring)
     return scoring
